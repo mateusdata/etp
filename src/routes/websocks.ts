c745a0a08ba8b519dc16fc7e3204a9c074c25f00
Mateus Santos <mateuspele2015@gmail.com>
2024-03-02 12:16:39 -0300

    added: web socks

--- src/routes/websocks.ts
+++ src/routes/websocks.ts
@@ -1,31 +1,32 @@
 import WebSocket from 'ws';
-let total = 0 
 
 const handleWebSocket = (wss: WebSocket.Server) => {
-  wss.on('connection', (ws) => {
-    console.log('Client connected');
-    ws.send("Você conectou em mim");
+    let total = 0
 
-    ws.on('message', (message) => {
-      console.log(`mensagem recebida: ${message}`);
+    wss.on('connection', (ws) => {
+        console.log('Client connected');
+        ws.send("Você conectou em mim");
 
-      ws.send(`Echo: ${message}`);
+        ws.on('message', (message) => {
+            console.log(`mensagem recebida: ${message}`);
+
+            ws.send(`Echo: ${message}`);
+        });
+
+        ws.on('close', () => {
+            console.log('Client disconnected');
+        });
+
+        const interval = setInterval(() => {
+            ws.send('Mensagens respondida => ' + String(total+1));
+            console.log('mensagem enviada ' + String(total++))
+        }, 1);
+
+        ws.on('close', () => {
+            console.log('Client disconnected');
+            clearInterval(interval);
+        });
     });
-
-    ws.on('close', () => {
-      console.log('Client disconnected');
-    });
-
-    const interval = setInterval(() => {
-      ws.send('Mensagens respondida => '+ total++);
-      console.log('mensagem enviada' + total++)
-    }, 1);
-
-    ws.on('close', () => {
-      console.log('Client disconnected');
-      clearInterval(interval);
-    });
-  });
 };
 
 export { handleWebSocket };
