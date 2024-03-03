010ea61677568db9498976d223eea33441fb11d3
Mateus Santos <mateuspele2015@gmail.com>
2024-03-03 01:48:41 -0300

    added: prisma schema

---
+++ prisma/migrations/20240303042150_create_chat/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "conversa" (
+    "id" SERIAL NOT NULL,
+    "id_usu1" INTEGER NOT NULL,
+    "id_usu2" INTEGER NOT NULL,
+
+    CONSTRAINT "conversa_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "mensagem" (
+    "id" SERIAL NOT NULL,
+    "remetente" INTEGER NOT NULL,
+    "destinatario" INTEGER NOT NULL,
+    "item" TEXT NOT NULL,
+    "id_conversa" INTEGER NOT NULL,
+
+    CONSTRAINT "mensagem_pkey" PRIMARY KEY ("id")
+);
